b8780a906f77a885457002fb379e713ca8333bce
Dima Vyshniakov <morewings@gmail.com>
2017-07-21 17:50:05 +0200

    add lint-staged

--- pre-commit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# This script tries to autofix lint errors in staged files and then stages them again.
-git diff --staged --diff-filter=dx --name-only HEAD | grep -e ".*\.js$" -e ".*\.scss$" -e ".*\.jsx $" | xargs -I % sh -c './node_modules/.bin/eslint --fix %; git add %'
