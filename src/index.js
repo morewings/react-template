a4ce22e5f38086bbd2a6c21377f38a754ad38042
Dima Vishnyakov <morewings@gmail.com>
2018-01-13 20:01:31 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'App';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-// Redux
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
-import ReduxThunk from 'redux-thunk';
-import rootReducer from 'Redux/reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(ReduxThunk, promiseMiddleware())),
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
