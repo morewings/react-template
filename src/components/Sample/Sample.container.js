894d0809852a97aaa0b8f7005ac931877a5197d3
Dima Vyshniakov <morewings@gmail.com>
2018-05-16 19:46:36 +0200

    update

--- src/components/Sample/Sample.container.js
+++ src/components/Sample/Sample.container.js
@@ -1,9 +1,9 @@
 import Sample from 'components/Sample/Sample';
 
 // redux
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { sample } from 'Redux/actions/index';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {sample} from 'Redux/actions/index';
 
 function mapStateToProps(state, ownProps) {
   return {
@@ -12,7 +12,7 @@ function mapStateToProps(state, ownProps) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ sample }, dispatch);
+  return bindActionCreators({sample}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sample);
