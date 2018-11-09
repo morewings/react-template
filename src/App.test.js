cd4d7055e93c3d4f30475bb807d0f8c5e2e0806e
Dima Vyshniakov <dmytro.vyshniakov@signavio.com>
2018-11-09 14:58:39 +0100

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {shallow} from 'enzyme'; // TODO: use mount and store
+import toJson from 'enzyme-to-json';
+import App from 'App';
 
 it('renders without crashing', () => {
-  //  const div = document.createElement('div');
-  //  ReactDOM.render(<App />, div);
+  const wrapper = shallow(<App />);
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
